ef486f5739079ba75d1743e83192067198cfc124
JCasique <casiquejohann@gmail.com>
2017-04-09 22:34:36 +0200

    DateFormat example added

---
+++ SwiftExamples/SwiftExamples/DateFormat.swift
@@ -0,0 +1,9 @@
+//
+//  DateFormat.swift
+//  SwiftExamples
+//
+//  Created by Johann Casique on 09/04/2017.
+//  Copyright © 2017 Johann Casique. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftExamples/SwiftExamplesTests/SwiftExamplesTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftExamplesTests.swift
+//  SwiftExamplesTests
+//
+//  Created by Johann Casique on 09/04/2017.
+//  Copyright © 2017 Johann Casique. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftExamples
+
+class SwiftExamplesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
